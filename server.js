ab6adb0b438bae91bdb670a941eb957eb4b667f0
siddhi-1234 <siddhip979@gmail.com>
2024-07-23 16:54:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
-const http=require("http")
-const port = 8081;
+const express=require("express");
+
+const app=express();
+app.use(express.json());
+
+const port=8081;
 
 const toDoList=["learn","apply things","succed"];
 
-http
-.createServer((req,res)=>{
-    // call back func
-    const{method,url}=req;
-    // console.log(method,url);
-    if(url==="/todos"){
-        if(method==="GET"){
-            res.writeHead(200);
-            res.write(toDoList.toString());
-        }
-    }
-    res.end();
-    // res.writeHead(200,{"Content-Type":"text/html"});
-    // res.write("<h2>hey server started:-)</h2>");
-    // res.end();
-})
-.listen(port,()=>{
-    // callback func
-    console.log(`NodeJs Server Started Running on Port ${port}`);
+app.get("/tods",(req,res)=>{
+    res.status(200).send(toDoList);
 });
 
-// http://localhost:8081
-
+app.listen(port,()=>{
+    console.log(`NodeJs Server Started Running On Port ${port}`);
+})
